2120867528190ca50f00c291ed9fa8c9913a7152
Vishal <87421912+vishalpatil12014017@users.noreply.github.com>
2023-05-09 18:19:39 +0530

    Add files via upload

---
+++ precache-manifest.0d1ebf107a14e3f237b7b8359769c090.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a8257bedd1116f6bfdc",
+    "url": "/static/css/main.2b6b75a2.chunk.css"
+  },
+  {
+    "revision": "6a8257bedd1116f6bfdc",
+    "url": "/static/js/main.6a8257be.chunk.js"
+  },
+  {
+    "revision": "956090ea75eddc4a5476",
+    "url": "/static/js/1.956090ea.chunk.js"
+  },
+  {
+    "revision": "622a60902f7cb2f7bc07",
+    "url": "/static/css/2.cd1337bd.chunk.css"
+  },
+  {
+    "revision": "622a60902f7cb2f7bc07",
+    "url": "/static/js/2.622a6090.chunk.js"
+  },
+  {
+    "revision": "fdc7311a411c721ea0c3",
+    "url": "/static/js/runtime~main.fdc7311a.js"
+  },
+  {
+    "revision": "078653eb59f4d30f9b9e2be0ae4014c9",
+    "url": "/index.html"
+  }
+];
